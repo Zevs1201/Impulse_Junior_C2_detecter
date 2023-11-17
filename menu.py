20462bf14e805234cb546ff37101b106a3400605
Zevs1201 <138981192+Zevs1201@users.noreply.github.com>
2023-11-17 20:15:14 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -47,77 +47,40 @@ __/ /___     // / /  / /   //         ((___( (   //     //___) )   ((____       
                                                                                       
 
 """)
-    print("\nPlease choose an option:")
-    print("1. start scanning, enter interface")
-    print("2. stop scanning")
-    print("3. Exit")
-
-class StoppableThread(threading.Thread):
-    def __init__(self, target):
-        super(StoppableThread, self).__init__(target=target)
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
 def main():
     global Analysis, Zeek, Proxy
-    Zeek = StoppableThread(target=run_zeek)
-    Analysis = StoppableThread(target=run_analysis)
-    Proxy = StoppableThread(target=run_proxy)
+    interface = input("enter the interface: ")
+    Analysis = Thread(target=run_analysis)
+    Zeek = Thread(target=run_zeek(interface))
+    Proxy = Thread(target=run_proxy)
+    display_menu()
+    Proxy.start()
+    Zeek.start()
+    Analysis.start()
 
-    while True:
-        display_menu()
-        choice = input("Enter your choice (1/2/3): ")
 
-        if choice == '1':
-            if not Proxy.is_alive():
-                Proxy.start()
-            if not Zeek.is_alive():
-                Zeek.start()
-                time.sleep(1)
-            if not Analysis.is_alive():
-                Analysis.start()
-        elif choice == '2':
-            Proxy.stop()
-            Zeek.stop()
-            Analysis.stop()
-        elif choice == '3':
-            print("Exiting...")
-            Proxy.stop()
-            Zeek.stop()
-            Analysis.stop()
-            break
-        else:
-            print("Invalid choice. Please enter 1, 2, or 3.")
 
 
 def run_analysis():
-    while not Analysis.stopped():
-        time.sleep(150)
-        while True:
-            os.system("python3 analiz_core_zeek.py")
-            time.sleep(60)  
-        pass
+    time.sleep(300)
+    while True:
+        os.system("python3 analiz_core_zeek.py")
+        time.sleep(60)
+    pass
 
-def run_zeek():
-    
-    interface = input("enter the interface:")
-    while not Zeek.stopped():
-        try:
-            subprocess.run(["zeek", "-i", interface], check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"Произошла ошибка при запуске Zeek: {e}")
-        except Exception as e:
-            print(f"Неожиданная ошибка: {e}")
-        pass
+
+def run_zeek(interface):
+    try:
+        subprocess.run(["zeek", "-i", interface], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Произошла ошибка при запуске Zeek: {e}")
+    except Exception as e:
+        print(f"Неожиданная ошибка: {e}")
+
+        
 
 def run_proxy():
-    while not Proxy.stopped():
-        os.system("python3 ban_ip.py")
-        pass
+    os.system("python3 ban_ip.py")
 
 
 
